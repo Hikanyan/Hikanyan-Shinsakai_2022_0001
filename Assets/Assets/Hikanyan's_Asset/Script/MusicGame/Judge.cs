ef78ed313341067c86b8eac250d979f01e82b2c6
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2022-06-12 01:37:39 +0900

    UI

--- Assets/Assets/Hikanyan's_Asset/Script/MusicGame/Judge.cs
+++ Assets/Assets/Hikanyan's_Asset/Script/MusicGame/Judge.cs
@@ -9,14 +9,15 @@ public class Judge : MonoBehaviour
     [SerializeField] NotesManager _notesManager;       //スクリプト「_notesManager」を入れる変数
     [SerializeField] Timer _timer;                     //スクリプト「_timer」を入れる関数
     private float _time = 0;
+    private bool _autoMode = false;
     private void Update()
     {
-        _time+=Time.deltaTime;
+        _time += Time.deltaTime;
 
 
         if (Input.GetKeyDown(KeyCode.D))//もし、Dキーが押されたとき
         {
-            if(_notesManager._notesTime.Count > 0 && _notesManager._laneNum[0] == 0)
+            if (_notesManager._notesTime.Count > 0 && _notesManager._laneNum[0] == 0)
             /*
              * _notesManager._notesTime.Countが0以上かつ
              * _notesManager._laneNum[0](レーン0)が0のとき
@@ -34,21 +35,21 @@ public class Judge : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.F))//もし、Fキーが押されたとき
         {
-            
+
             if (_notesManager._notesTime.Count > 0 && _notesManager._laneNum[0] == 1)
             {
                 Judgment(GetABS(Timer.instance.RealTime - _notesManager._notesTime[0]));
-                
+
             }
             Debug.Log($"Fキーが押された{_time}");
         }
         if (Input.GetKeyDown(KeyCode.J))//もし、Jキーが押されたとき
         {
-            
+
             if (_notesManager._notesTime.Count > 0 && _notesManager._laneNum[0] == 2)
             {
                 Judgment(GetABS(Timer.instance.RealTime - _notesManager._notesTime[0]));
-                
+
             }
             Debug.Log($"Jキーが押された{_time}");
         }
@@ -57,7 +58,7 @@ public class Judge : MonoBehaviour
             if (_notesManager._notesTime.Count > 0 && _notesManager._laneNum[0] == 3)
             {
                 Judgment(GetABS(Timer.instance.RealTime - _notesManager._notesTime[0]));
-                
+
             }
             Debug.Log($"Kキーが押された{_time}");
         }
@@ -74,8 +75,8 @@ public class Judge : MonoBehaviour
                 Debug.Log($"みす{_time}");
             }
         }
-        /*
-        if (_notesManager._notesTime.Count > 0)//オート
+
+        if (_autoMode == true && _notesManager._notesTime.Count > 0)//オート
         {
 
             if (Timer.instance.RealTime > _notesManager._notesTime[0])
@@ -84,11 +85,11 @@ public class Judge : MonoBehaviour
                 ScoreText.Instance._auto++;
                 NotesMessage(0);
                 DeleteData();
-                //ミス
+                Debug.Log($"オート{_time}");
                 Debug.Log($"オート");
             }
         }
-        */
+
     }
 
 
@@ -96,7 +97,7 @@ public class Judge : MonoBehaviour
 
 
 
-    void Judgment (float timeLag)//後から判定を増やすのもあり
+    void Judgment(float timeLag)//後から判定を増やすのもあり
     {
         if (timeLag < 0.10)//もし、本来のノーツを叩くべき時間と実際にノーツを叩いた時間の誤差が0.1秒以下だったら
         {
@@ -112,13 +113,13 @@ public class Judge : MonoBehaviour
             NotesMessage(1);
             DeleteData();
         }
-        
+
     }
 
     float GetABS(float num)//因数の絶対値を返す関数
-                          //ABSは絶対値 (absolute value) を返す関数という意味
+                           //ABSは絶対値 (absolute value) を返す関数という意味
     {
-        if(num >= 0)//もし、numが0より大きかったらnumを返す（0以上）
+        if (num >= 0)//もし、numが0より大きかったらnumを返す（0以上）
         {
             return num;
         }
@@ -130,7 +131,7 @@ public class Judge : MonoBehaviour
 
     void DeleteData()//叩いたノーツを非表示もしくは削除する関数
     {
-        if(_notesManager._notesTime.Count > 0)//_notesManager.NotesTimeが1以上なら実行
+        if (_notesManager._notesTime.Count > 0)//_notesManager.NotesTimeが1以上なら実行
         {
             _notesManager._notesTime.RemoveAt(0);
             _notesManager._laneNum.RemoveAt(0);
@@ -140,6 +141,6 @@ public class Judge : MonoBehaviour
 
     void NotesMessage(int judge)//ノーツを叩いた時の判定の表示
     {
-        Instantiate(_judgeObjects[judge],new Vector3(_notesManager._laneNum[0]-1.5f,0.76f,0.15f),Quaternion.Euler(45,0,0));
+        Instantiate(_judgeObjects[judge], new Vector3(_notesManager._laneNum[0] - 1.5f, 0.76f, 0.15f), Quaternion.Euler(45, 0, 0));
     }
 }
